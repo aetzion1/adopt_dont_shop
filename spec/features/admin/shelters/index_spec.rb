6a4f869fdc1bacb694f98af062aa0360f6f68a8b
Adam Etzion <adametzion90@gmail.com>
2020-12-17 17:25:44 -0700

    Add test for pending shelters

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -5,11 +5,12 @@ RSpec.describe "Admin Shelters Index" do
         @dans = Shelter.create!(name: "Dan's Doghouse", address: "123 Hellhound St", city: "Dogtown", state: "CA", zip: 66666)
         @kathy = Shelter.create!(name: "Kathy's Kathouse", address: "1 Rio Drive", city: "Beverly Hills", state: "CA", zip: 90210)
         @stinky = Shelter.create!(name: "Ms. Stinky's", address: "165 12th St", city: "Boston", state: "MA", zip: 87655)
+
+        
     end
     describe "As a visitor, I visit /admin/shelters" do
         it "Displays all Shelters in reversed alphabetical order by name" do
             visit '/admin/shelters'
-            save_and_open_page
 
             expect(page.all("#shelters")[2]).to have_content(@dans.name)
             expect(page.all("#shelters")[2]).to have_content(@dans.address)
@@ -25,6 +26,14 @@ RSpec.describe "Admin Shelters Index" do
             expect(page.all("#shelters")[0]).to have_content(@stinky.address)
             expect(page.all("#shelters")[0]).to have_content(@stinky.state)
             expect(page.all("#shelters")[0]).to have_content(@stinky.zip)
+        end
+
+        it "displays a section for shelters with pending applications" do
+            visit '/admin/shelters'
+            save_and_open_page
+
+            expect(page.all("#pending").to have_content(@dans.name)
+
         end
     end
 end
